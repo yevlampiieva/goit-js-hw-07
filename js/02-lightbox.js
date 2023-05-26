1f405451d2c59ab25d3bd713e0c1a5cf9c6aaa93
Alla Yevlampiieva <modan1679@gmail.com>
2023-05-26 03:17:31 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const list = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description}/>
+      </a>
+      </li>`
+  )
+  .join("");
+
+list.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
