bd406a6c3ae3ee42fbcabd6cb03f381da167c9df
Alla Yevlampiieva <modan1679@gmail.com>
2025-05-18 00:41:41 +0300

    Do task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,12 @@
+const changeColorBtn = document.querySelector(".change-color");
+const changeColorText = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  changeColorText.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
