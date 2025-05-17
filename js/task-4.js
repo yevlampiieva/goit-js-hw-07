81953517b402176b592e0516311d8aad00662543
Alla Yevlampiieva <modan1679@gmail.com>
2025-05-18 00:21:16 +0300

    Do task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userEmail = e.target.elements.email.value.trim();
+  const userPassword = e.target.elements.password.value.trim();
+  if (userEmail === "" || userPassword === "") {
+    alert("All form fields must be filled in");
+  } else {
+    console.log({
+      email: userEmail,
+      password: userPassword,
+    });
+    formEl.reset();
+  }
+});
